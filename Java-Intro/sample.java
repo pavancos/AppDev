9300e4ac6b90c5192b94a2d4d83c48957ad01f5e
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-11-22 15:26:02 +0530

    init: repo and add java intro

---
+++ Java-Intro/sample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class sample{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter any string");
+        String str= s.nextLine();
+        System.out.println("You entered: "+str);
+        s.close();
+    }
+}
