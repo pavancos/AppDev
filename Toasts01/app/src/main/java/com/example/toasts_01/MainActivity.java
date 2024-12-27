1abf1fa6d383cc9e23cc32c3b47f444fa24aa905
pavancos <pavankc005@gmail.com>
2024-12-27 10:39:36 +0530

    feat: fin Custom Toasts

--- Toasts01/app/src/main/java/com/example/toasts_01/MainActivity.java
+++ Toasts01/app/src/main/java/com/example/toasts_01/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.toasts_01;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -29,64 +33,64 @@ public class MainActivity extends AppCompatActivity {
         ).show();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(
-                this,
-                "This is onResume()",
-                Toast.LENGTH_SHORT
-        ).show();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(
-                this,
-                "This is onPause()",
-                Toast.LENGTH_SHORT
-        ).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(
-                this,
-                "This is onStop()",
-                Toast.LENGTH_SHORT
-        ).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(
-                this,
-                "This is onDestroy()",
-                Toast.LENGTH_SHORT
-        ).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(
-                this,
-                "This is onRestart()",
-                Toast.LENGTH_SHORT
-        ).show();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(
-                this,
-                "This is onStart()",
-                Toast.LENGTH_SHORT
-        ).show();
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Toast.makeText(
+//                this,
+//                "This is onResume()",
+//                Toast.LENGTH_SHORT
+//        ).show();
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Toast.makeText(
+//                this,
+//                "This is onPause()",
+//                Toast.LENGTH_SHORT
+//        ).show();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Toast.makeText(
+//                this,
+//                "This is onStop()",
+//                Toast.LENGTH_SHORT
+//        ).show();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Toast.makeText(
+//                this,
+//                "This is onDestroy()",
+//                Toast.LENGTH_SHORT
+//        ).show();
+//    }
+//
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Toast.makeText(
+//                this,
+//                "This is onRestart()",
+//                Toast.LENGTH_SHORT
+//        ).show();
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Toast.makeText(
+//                this,
+//                "This is onStart()",
+//                Toast.LENGTH_SHORT
+//        ).show();
+//    }
 
     public void showText(View view){
         Toast.makeText(
@@ -94,6 +98,17 @@ public class MainActivity extends AppCompatActivity {
                 "This is on Click",
                 Toast.LENGTH_SHORT
         ).show();
+    }
+
+    public void showCustomToast(View view){
+        LayoutInflater li = getLayoutInflater();
+        ViewGroup layout = findViewById(R.id.custom_toast);
+        View layoutView = li.inflate(R.layout.custom_toast, layout);
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM, 0, 300);
+        toast.setView(layoutView);
+        toast.show();
     }
 
 
